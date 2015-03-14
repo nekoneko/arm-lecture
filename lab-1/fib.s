252b6bd1a8fff4fc9d00857b1aa3255a85d7066c
nekonkeo <kk357258@gmail.com>
2015-03-14 11:31:21 +0800

    condition branch before stack operation

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -11,19 +11,20 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+
+	cmp r0, #0
+	itt le
+	movle r0, #0
+	bxle lr
+
+	cmp r0, #1
+	it eq
+	bxeq lr
+
+	push {r4, r5, lr}
 
 	@ R4 = R0 - 0 (update flags)
 	subs 	r4, r0, #0
-
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-	cmp 	r0, #0
-	ble 	.L3
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-	cmp 	r4, #1
-	beq 	.L4
 
 	@ R0 = R4 - 1
 	@ Recursive call to fibonacci with R4 - 1 as parameter
@@ -40,16 +41,9 @@ fibonacci:
 	@ R0 = R5 + R0 (update flags)
 	adds 	r0, r0, r5
 
-	pop {r3, r4, r5, pc}		@EPILOG
+	pop {r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
